83f5cbcf860ff126edc4d6a41dc2f0c829b0b0e1
jalbee <jesse@jalbee.com>
2013-10-03 12:25:22 -0700

    changed preferences and removed items unrelated to my workflow

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,7 @@
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+# bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
@@ -12,7 +12,7 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 # https://github.com/isaacs/nave
 # needs npm, obviously.
 # TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
+# npm install -g nave
 
 
 # homebrew!
@@ -40,21 +40,3 @@ git clone git://github.com/dronir/SpotifyControl.git
 # VLC Controll Script
 cd ~/code
 git clone git://github.com/jeroenbegyn/VLCControl.git
-
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
-
